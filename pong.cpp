e2bf4acfc476cc632f2536f58c949c4468d712a8
BuildTools <unconfigured@null.spigotmc.org>
2026-08-14 20:20:11 +0100

    Renders two paddles and a ball, no additional functionality yet.

---
+++ pong.cpp
@@ -0,0 +1,119 @@
+#include <string>
+#include <vector>
+#include <SFML/Graphics.hpp>
+#include <SFML/Window.hpp>
+#include <SFML/System.hpp>
+#include <cmath>
+#include <iostream>
+
+using namespace std;
+using namespace sf;
+
+
+
+struct {
+    unsigned int screenWidth = 720;
+    unsigned int screenHeight = 480;
+
+    const float paddleBuffer = screenWidth/12;
+
+    const float FPS = 60;
+
+}Config;
+
+class Ball {
+    public:
+        float radius = 5;
+
+        float x = Config.screenWidth/2;
+        float y = Config.screenHeight/2;
+
+        float vx;
+        float vy;
+
+        CircleShape shape;
+
+    Ball(){
+
+        shape = CircleShape(radius);
+        shape.setOrigin({radius,radius});
+        shape.setPosition({x, y});
+        shape.setFillColor(Color(255,255,255));
+
+    }
+
+};
+
+class Paddle {
+    public:
+        bool leftOrRight;
+
+        float x;
+        float y = Config.screenHeight/2;
+
+        float length = 5;
+        float width = Config.screenHeight/8;
+
+        float vy;
+
+        RectangleShape shape;
+
+    Paddle(bool side){
+
+        if (side){
+            x = Config.paddleBuffer; //IF SIDE IS TRUE/ONE PADDLE IS ON THE LEFT
+        } else {
+            x = Config.screenWidth - Config.paddleBuffer; //IF SIDE IS FALSE/ZERO PADDLE IS ON RIGHT
+        }
+
+
+        shape = RectangleShape({length, width});
+        shape.setOrigin({length/2, width/2});
+        shape.setPosition({x, y});
+        shape.setFillColor(Color(255,255,255));
+    }
+
+};
+
+
+
+int main(){
+    float dt = 1.0f/Config.FPS;
+
+    
+    Ball ball{};
+
+    Paddle rightPaddle(0);
+    Paddle leftPaddle(1);
+    
+
+
+    RenderWindow* window = new RenderWindow(VideoMode({Config.screenWidth, Config.screenHeight}), "Pong");
+
+        window->setFramerateLimit(Config.FPS);
+        
+        while (window->isOpen()){ 
+            while (const optional event = window->pollEvent()){
+                if (event->is<Event::Closed>()){
+                    window->close();
+                }
+         
+         
+            }
+            //PUT GAME LOOP HERE
+            
+
+        
+
+            window->clear();
+
+            //RENDER
+            window->draw(ball.shape);
+            window->draw(leftPaddle.shape);
+            window->draw(rightPaddle.shape);
+
+
+            window->display();
+        }    
+    return 0;
+}
